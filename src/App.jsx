37f8a60eb8b7742891b1f6905ebd42923d5c7e9a
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-04-25 11:02:50 +0600

    project primary setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-green-800">User Management System</h1>
+      <input
+        type="text"
+        placeholder="Warning"
+        className="input input-warning"
+      />
+    </>
+  );
+}
+
+export default App;
